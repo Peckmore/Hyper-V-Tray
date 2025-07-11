9fa813fcd3fb82c5fed0796d35e2a01da947049a
Simon Peckmore <11754624+Peckmore@users.noreply.github.com>
2025-07-11 14:32:07 +0100

    Resources cleanup.

--- src/Hyper-V Tray/ResourceHelper.cs
+++ src/Hyper-V Tray/ResourceHelper.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Specialized;
+﻿using HyperVTray.Resources;
+using System;
 using System.Drawing;
 using System.IO;
 using System.Reflection;
@@ -9,12 +9,6 @@ namespace HyperVTray
 {
     internal static class ResourceHelper
     {
-        #region Constants
-
-        private static readonly StringDictionary StringsDictionary;
-
-        #endregion
-
         #region Fields
 
         private static ResourceManager? _vmBrowserResourceManager;
@@ -25,37 +19,39 @@ namespace HyperVTray
 
         static ResourceHelper()
         {
-            StringsDictionary = new();
-            Icon_HyperV = GetIconResource(Resources.Icon_HyperV);
+            Icon_HyperV = GetIconResource(Icons.TrayIcon_Modern);
         }
 
         #endregion
 
         #region Properties
 
-        internal static string Command_Connect => GetStringResource("VMOpen_Name", Resources.Command_Connect);
-        internal static string Command_Pause => GetStringResource("VMPause_Name", Resources.Command_Pause);
-        internal static string Command_Reset => GetStringResource("VMReset_Name", Resources.Command_Reset);
-        internal static string Command_Resume => GetStringResource("VMResume_Name", Resources.Command_Resume);
-        internal static string Command_Save => GetStringResource("VMSaveState_Name", Resources.Command_Save);
-        internal static string Command_ShutDown => GetStringResource("VMShutDown_Name", Resources.Command_ShutDown);
-        internal static string Command_Start => GetStringResource("VMStart_Name", Resources.Command_Start);
-        internal static string Command_TurnOff => GetStringResource("VMTurnOff_Name", Resources.Command_TurnOff);
+        internal static string Command_Connect => GetStringResource("VMOpen_Name", StringsFallback.Command_Connect);
+        internal static string Command_Pause => GetStringResource("VMPause_Name", StringsFallback.Command_Pause);
+        internal static string Command_Reset => GetStringResource("VMReset_Name", StringsFallback.Command_Reset);
+        internal static string Command_Resume => GetStringResource("VMResume_Name", StringsFallback.Command_Resume);
+        internal static string Command_Save => GetStringResource("VMSaveState_Name", StringsFallback.Command_Save);
+        internal static string Command_ShutDown => GetStringResource("VMShutDown_Name", StringsFallback.Command_ShutDown);
+        internal static string Command_Start => GetStringResource("VMStart_Name", StringsFallback.Command_Start);
+        internal static string Command_TurnOff => GetStringResource("VMTurnOff_Name", StringsFallback.Command_TurnOff);
         internal static Icon Icon_HyperV { get; }
-        internal static string Message_PauseVMFailed => GetStringResource("Message_PauseVMFailed", Resources.Message_PauseVMFailed);
-        internal static string Message_PowerOffVMFailed => GetStringResource("Message_PowerOffVMFailed", Resources.Message_PowerOffVMFailed);
-        internal static string Message_ResetVMFailed => GetStringResource("Message_ResetVMFailed", Resources.Message_ResetVMFailed);
-        internal static string Message_ResumeVMFailed => GetStringResource("Message_ResumeVMFailed", Resources.Message_ResumeVMFailed);
-        internal static string Message_SaveStateVMFailed => GetStringResource("Message_SaveStateVMFailed", Resources.Message_SaveStateVMFailed);
-        internal static string Message_ShutDownVMFailed => GetStringResource("Message_ShutDownVMFailed", Resources.Message_ShutDownVMFailed);
-        internal static string Message_StartVMFailed => GetStringResource("Message_StartVMFailed", Resources.Message_StartVMFailed);
-        internal static string State_Critical => GetStringResource("cccccccc", Resources.State_Critical);
-        internal static string State_Off => GetStringResource("fffffff", Resources.State_Off);
-        internal static string State_Paused => GetStringResource("DDDDDD", Resources.State_Paused);
-        internal static string State_Running => GetStringResource("rrrrrrr", Resources.State_Running);
-        internal static string State_Saved => GetStringResource("dddddddd", Resources.State_Saved);
-        internal static string Toast_CriticalState => Resources.Toast_CriticalState;
-        
+        internal static string Menu_AllVirtualMachines => GetStringResource("Menu_AllVirtualMachines", Strings.Menu_AllVirtualMachines);
+        internal static string Menu_HyperVManager => GetStringResource("SnapInNode_DisplayName", StringsFallback.Menu_HyperVManager);
+        internal static string Message_PauseVMFailed => GetStringResource("Message_PauseVMFailed", StringsFallback.Message_PauseVMFailed);
+        internal static string Message_PowerOffVMFailed => GetStringResource("Message_PowerOffVMFailed", StringsFallback.Message_PowerOffVMFailed);
+        internal static string Message_ResetVMFailed => GetStringResource("Message_ResetVMFailed", StringsFallback.Message_ResetVMFailed);
+        internal static string Message_ResumeVMFailed => GetStringResource("Message_ResumeVMFailed", StringsFallback.Message_ResumeVMFailed);
+        internal static string Message_SaveStateVMFailed => GetStringResource("Message_SaveStateVMFailed", StringsFallback.Message_SaveStateVMFailed);
+        internal static string Message_ShutDownVMFailed => GetStringResource("Message_ShutDownVMFailed", StringsFallback.Message_ShutDownVMFailed);
+        internal static string Message_StartVMFailed => GetStringResource("Message_StartVMFailed", StringsFallback.Message_StartVMFailed);
+        internal static string State_Critical => GetStringResource("cccccccc", StringsFallback.State_Critical);
+        internal static string State_Off => GetStringResource("fffffff", StringsFallback.State_Off);
+        internal static string State_Paused => GetStringResource("DDDDDD", StringsFallback.State_Paused);
+        internal static string State_Running => GetStringResource("rrrrrrr", StringsFallback.State_Running);
+        internal static string State_Saved => GetStringResource("dddddddd", StringsFallback.State_Saved);
+        internal static string Toast_CriticalState => StringsFallback.Toast_CriticalState;
+        internal static string String_UnknownVirtualMachine => StringsFallback.String_UnknownVirtualMachine;
+
         #endregion
 
         #region Methods
@@ -71,12 +67,7 @@ namespace HyperVTray
         }
         private static string GetStringResource(string resourceName, string fallbackValue)
         {
-            if (!StringsDictionary.ContainsKey(resourceName))
-            {
-                StringsDictionary[resourceName] = _vmBrowserResourceManager?.GetString(resourceName) ?? fallbackValue;
-            }
-
-            return StringsDictionary[resourceName]!;
+            return _vmBrowserResourceManager?.GetString(resourceName) ?? fallbackValue;
         }
 
         #endregion
